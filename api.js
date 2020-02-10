74192eecc23f8ec6ef2d5aac840cf2d2289199e1
David Lundholm <david.lundholm@zocom.se>
2020-02-10 08:39:16 +0100

    Added CORS middleware

--- api.js
+++ api.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const noteRouter = require('./routes/notes')
 const PORT = 5000
 
+
+app.use(cors())
 app.use(express.json())
 app.use('/api/notes', noteRouter)
 
